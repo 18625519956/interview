1f493bb4f7b95dccf82178a4847cad94ac6760aa
zhangqi <18625519956@163.com>
2021-01-27 23:43:30 +0800

    张启：包结构完成

--- src/main/java/com/qichen/interview/interview/entity/User.java
+++ src/main/java/com/qichen/interview/interview/entity/User.java
@@ -4,6 +4,14 @@ public class User {
     private String name;
     private String password;
 
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
     public String getName() {
         return name;
     }
@@ -18,5 +26,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
